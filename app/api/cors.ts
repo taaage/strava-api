049bbf810415e1e9b7462ce9a21f2da60a4c095d
Tigge Nilsson <tigge.nilsson@ericsson.com>
2026-08-22 19:57:34 +0200

    Add allowed origin for CORS configuration

--- app/api/cors.ts
+++ app/api/cors.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 const ALLOWED_ORIGINS = [
   "https://strava.tiggenilsson.se",
+  "https://tiggenilsson.se",
   "http://localhost:5173",
   "http://localhost:3000",
 ];
